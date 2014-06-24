ed3e30142f7fd9adeb5e82d36b214da60014e81f
Ramprasad Rajendran <r.ramprasad@gmail.com>
2014-06-24 18:48:13 +0530

    Add Support for caching, showing interstitials
    Plus associated callbacks, adavailable, not available, ad dismissed
    etc..

--- android/LeadBoltPlugin.java
+++ android/LeadBoltPlugin.java
@@ -26,11 +26,80 @@ import com.tealeaf.EventQueue;
 import com.tealeaf.event.*;
 
 import LEADBOLT_PACKAGE.AdController;
+import LEADBOLT_PACKAGE.AdListener;
 
 public class LeadBoltPlugin implements IPlugin {
 	Context _ctx;
 
-	private AdController myController;
+	private AdController ad;
+	private Activity mActivity;
+
+	public class LeadboltAdNotAvailable extends com.tealeaf.event.Event {
+
+		public LeadboltAdNotAvailable() {
+			super("LeadboltAdNotAvailable");
+		}
+	}
+
+	public class LeadboltAdAvailable extends com.tealeaf.event.Event {
+
+		public LeadboltAdAvailable() {
+			super("LeadboltAdAvailable");
+		}
+	}
+
+	public class LeadboltAdDismissed extends com.tealeaf.event.Event {
+
+		public LeadboltAdDismissed() {
+			super("LeadboltAdDismissed");
+		}
+	}
+
+	private class PluginDelegate implements AdListener {
+		@Override
+		public void onAdLoaded() {
+		}
+
+		@Override
+		public void onAdClicked() {
+			EventQueue.pushEvent(new LeadboltAdDismissed());
+		}
+
+		@Override
+		public void onAdClosed() {
+			EventQueue.pushEvent(new LeadboltAdDismissed());
+		}
+
+		@Override
+		public void onAdCompleted() {
+		}
+
+		@Override
+		public void onAdFailed() {
+			EventQueue.pushEvent(new LeadboltAdNotAvailable());
+		}
+
+		@Override
+		public void onAdProgress() {
+		}
+
+		@Override
+		public void onAdAlreadyCompleted() {
+		}
+
+		@Override
+		public void onAdPaused() {
+		}
+
+		@Override
+		public void onAdResumed() {
+		}
+
+		@Override
+		public void onAdCached() {
+			EventQueue.pushEvent(new LeadboltAdAvailable());
+		}
+	}
 
 	public LeadBoltPlugin() {
 	}
@@ -40,12 +109,23 @@ public class LeadBoltPlugin implements IPlugin {
 	}
 
 	public void onCreate(Activity activity, Bundle savedInstanceState) {
+		mActivity = activity;
+
 		String leadBoltPackage = "LEADBOLT_PACKAGE";
 
 		logger.log("{leadbolt} Initializing LeadBolt ad framework with package:", leadBoltPackage);
 
-		myController = new AdController(activity, "MY_LB_SECTION_ID");
-		myController.loadStartAd("MY_LB_NOTIFICATION_ID", "MY_LB_ICON_ID");
+		this.ad = new AdController(mActivity, "MY_LB_SECTION_ID", new PluginDelegate());
+		//myController = new AdController(activity, "MY_LB_SECTION_ID");
+		//myController.loadStartAd("MY_LB_NOTIFICATION_ID", "MY_LB_ICON_ID");
+	}
+
+	public void showInterstitial(String jsonData) {
+		this.ad.loadAd();
+	}
+
+	public void cacheInterstitial(String jsonData) {
+		this.ad.loadAdToCache();
 	}
 
 	public void onResume() {
@@ -61,7 +141,7 @@ public class LeadBoltPlugin implements IPlugin {
 	}
 
 	public void onDestroy() {
-		myController.destroyAd();
+		this.ad.destroyAd();
 	}
 
 	public void onNewIntent(Intent intent) {
@@ -83,4 +163,3 @@ public class LeadBoltPlugin implements IPlugin {
 	public void onBackPressed() {
 	}
 }
-
--- js/leadBolt.js
+++ js/leadBolt.js
@@ -1,12 +1,83 @@
+import util.setProperty as setProperty;
+
+var onAdDismissed, onAdAvailable, onAdNotAvailable;
+
 var LeadBolt = Class(function () {
+	this.init = function(opts) {
+
+		setProperty(this, "onAdDismissed", {
+			set: function(f) {
+				     if (typeof f === "function") {
+					     onAdDismissed = f;
+				     } else {
+					     onAdDismissed = null;
+				     }
+			     },
+			get: function() {
+				     return onOfferClose;
+			     }
+		});
+
+		setProperty(this, "onAdAvailable", {
+			set: function(f) {
+				     if (typeof f === "function") {
+					     onAdAvailable = f;
+				     } else {
+					     onAdAvailable = null;
+				     }
+			     },
+			get: function() {
+				     return onAdAvailable;
+			     }
+		});
+
+		setProperty(this, "onAdNotAvailable", {
+			set: function(f) {
+				     if (typeof f === "function") {
+					     onAdNotAvailable = f;
+				     } else {
+					     onAdNotAvailable = null;
+				     }
+			     },
+			get: function() {
+				     return onAdNotAvailable;
+			     }
+		});
+
+		NATIVE.events.registerHandler("LeadboltAdDismissed", function() {
+			logger.log("{leadbolt} ad dismissed ");
+			if (typeof onAdDismissed === "function") {
+				onAdDismissed();
+			}
+		});
+
+		NATIVE.events.registerHandler("LeadboltAdAvailable", function() {
+			logger.log("{leadbolt} ad available");
+			if (typeof onAdAvailable === "function") {
+				onAdAvailable("leadbolt");
+			}
+		});
+
+		NATIVE.events.registerHandler("LeadboltAdNotAvailable", function() {
+			logger.log("{leadbolt} ad not available");
+			if (typeof onAdNotAvailable === "function") {
+				onAdNotAvailable();
+			}
+		});
+
+	}
+
 	this.showInterstitial = function () {
 		logger.log("{leadBolt} Showing interstitial");
 
-		if (NATIVE && NATIVE.plugins && NATIVE.plugins.sendEvent) {
-			NATIVE.plugins.sendEvent("LeadBoltPlugin", "leadBolt",
-				JSON.stringify({}));
-		}
-	};
+		NATIVE.plugins.sendEvent("LeadBoltPlugin", "showInterstitial", JSON.stringify({}));
+	}
+
+	this.cacheInterstitial = function () {
+		logger.log("{leadBolt} caching interstitial");
+
+		NATIVE.plugins.sendEvent("LeadBoltPlugin", "cacheInterstitial", JSON.stringify({}));
+	}
 });
 
 exports = new LeadBolt();
