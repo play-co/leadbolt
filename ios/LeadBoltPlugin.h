37a52df92a4940e7394b2bba3b3828236675592f
Ramprasad Rajendran <r.ramprasad@gmail.com>
2014-09-17 16:16:06 +0530

    LB Direct deals for iOS fallback to LB Global SDK
    Also upgraded to latest version

--- ios/LeadBoltPlugin.h
+++ ios/LeadBoltPlugin.h
@@ -1,9 +1,12 @@
 #import "PluginManager.h"
-#import "LeadBoltOverlay.h"
+#import "sdk/LeadBoltOverlay.h"
+#import "sdk/AppFireworks/AppTracker.h"
 
 @interface LeadBoltPlugin : GCPlugin
 
 @property (nonatomic, retain) LeadboltOverlay *myAdController;
+@property (nonatomic, retain) UIView *view;
+@property (nonatomic, retain) NSString *leadBoltSectionId;
 
 @end
 
